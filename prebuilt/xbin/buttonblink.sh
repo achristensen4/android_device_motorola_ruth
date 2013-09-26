eccee41cf30f5094d85dcb4802b58e9f7adfc3fc
phchshow <bixictn@gmail.com>
2013-09-26 13:09:19 +0800

    check

--- prebuilt/xbin/buttonblink.sh
+++ prebuilt/xbin/buttonblink.sh
@@ -1,14 +1,3 @@
 #!/system/bin/sh
-
-
-while [ true ]
-do
-    blink=$(cat /sys/class/leds/button-backlight/blink)
-    if [ "$blink" != "0x0" ]; then
-        brightness=$(cat /sys/class/leds/button-backlight/brightness)
-        if [ "$brightness" == "0" ]; then
-            echo 255 > /sys/class/leds/button-backlight/brightness
-        fi
-    fi
-    sleep 2s
-done
+sleep 2s
+echo 255 > /sys/class/leds/button-backlight/brightness
